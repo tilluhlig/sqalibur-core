d436fe4e3cf03d716cd1a5a2624bd80ed5d1fc60
Till Uhlig <till.uhlig@student.uni-halle.de>
2017-02-03 10:00:43 +0100

    no message

--- src/main/java/sqalibur/segments/rules/flatten.java
+++ src/main/java/sqalibur/segments/rules/flatten.java
@@ -1,18 +1,7 @@
-/* 
- * Copyright (C) 2017 Till Uhlig <till.uhlig@student.uni-halle.de>
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
  */
 package sqalibur.segments.rules;
 
@@ -23,10 +12,10 @@ import treeNormalizer.transformation;
  *
  * @author Till Uhlig <till.uhlig@student.uni-halle.de>
  */
-public class flatten extends rule{
+public class flatten extends rule {
     
     @Override
     public void perform(transformation transform) {
-    
+
     }
 }
